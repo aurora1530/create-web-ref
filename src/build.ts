6de54cfa1e3c1960763f5030b28e7cf9e1108e96
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-02-22 17:57:09 +0900

    複数ファイルを一つのbookmarkletにまとめるように変更

--- src/build.ts
+++ src/build.ts
@@ -1,19 +1,24 @@
 // @ts-ignore
 import { minify } from 'minify';
+import url from 'url';
 import fs from 'fs';
 import path from 'path';
 
-async function main(filePath: string): Promise<string> {
-  const minifiedJs = await minify(filePath);
-  return `javascript:(()=>{${minifiedJs}})()`;
+const fileOrder = ['ldJsonParser', 'index'];
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePaths = fileOrder.map((file) => path.join(__dirname, '../dist/', `${file}.js`));
+const outputPath = path.join(__dirname, '../out', 'bookmarklet.js');
+
+function toBookmarklet(minifiedJsCode: string): string {
+  return `javascript:(()=>{${minifiedJsCode}})()`;
 }
 
-const filePaths = process.argv.slice(2);
+async function main() {
+  const codes = filePaths.map((filePath) => fs.readFileSync(filePath, 'utf-8'));
+  fs.writeFileSync(outputPath, codes.join('\n'));
+  const minifiedJsCode = await minify(outputPath);
+  const bookmarklet = toBookmarklet(minifiedJsCode);
+  fs.writeFileSync(path.join(__dirname, '..', 'out', 'bookmarklet.js'), bookmarklet);
+}
 
-filePaths.forEach((filePath) => {
-  const dirPath = path.dirname(filePath);
-  const fileName = path.basename(filePath).replace(/\.js$/, '.min.js');
-  main(filePath).then((result) => {
-    fs.writeFileSync(path.join(dirPath.replace('dist', 'out'), fileName), result);
-  });
-});
+main();
